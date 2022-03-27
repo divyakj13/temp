83dd01c805216c2f5131df4976ca367dce694b8e
Divya KJ <73816949+divyakj13@users.noreply.github.com>
2022-03-28 00:12:58 +0530

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,12 @@
+const websocket = require('ws');
+const wss = new websocket.Server({
+    port:8000
+})
+wss.on('connection',function (socket) {
+    console.log("connection ready");
+
+    socket.on('message',function (msg) {
+        console.log("Client : "+msg);
+        socket.send("hi client");
+    })
+})
